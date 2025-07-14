dea330ef15e14f2a3bfa62b9834c7ab52152d3db
scanmonk <scanmonkgraphics@gmail.com>
2025-07-14 16:06:51 +0530

    sample commit

--- server/routes/user.js
+++ server/routes/user.js
@@ -23,16 +23,22 @@ router.patch("/:userId/:listingId", async (req, res) => {
     const user = await User.findById(userId)
     const listing = await Listing.findById(listingId).populate("creator")
 
-    const favoriteListing = user.wishList.find((item) => item._id.toString() === listingId)
+    const favoriteListing = user.wishList.find(
+      (item) =>
+        item && // check if item exists
+        (item._id ? item._id.toString() : item.toString()) === listingId
+
+        
+    )
 
     if (favoriteListing) {
       user.wishList = user.wishList.filter((item) => item._id.toString() !== listingId)
       await user.save()
-      res.status(200).json({ message: "Listing is removed from wish list", wishList: user.wishList})
+      res.status(200).json({ message: "Listing is removed from wish list", wishList: user.wishList })
     } else {
       user.wishList.push(listing)
       await user.save()
-      res.status(200).json({ message: "Listing is added to wish list", wishList: user.wishList})
+      res.status(200).json({ message: "Listing is added to wish list", wishList: user.wishList })
     }
   } catch (err) {
     console.log(err)
